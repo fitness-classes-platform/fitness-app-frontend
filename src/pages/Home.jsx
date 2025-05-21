69d409a0eadd02c721d1081b1e905dfcff098962
José Baldino <josemanuelbaldino@gmail.com>
2025-05-21 13:45:06 +0100

    fix home page bug to show classes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,33 +4,34 @@ import { useEffect, useState } from "react";
 
 
 function HomePage() {
-    const [data, setData]= useState([])
-    const [error, setError] = useState(null); 
+    const [data, setData] = useState([])
+    const [error, setError] = useState(null);
 
-useEffect(()=>{
-    axios.get(`${import.meta.env.VITE_API_URL}/api/class`)
-    .then((response)=>{
-        setData(response.data)
-    })
-    .catch((e) => {
-        console.log("Error finding cities", e);
-        setError("Failed to load cities. Please try again later.");
-      })
+    useEffect(() => {
+        axios.get(`${import.meta.env.VITE_API_URL}/api/class`)
+            .then((response) => {
+                setData(response.data)
+            })
+            .catch((e) => {
+                console.log("Error finding classes", e);
+                setError("Failed to load classes. Please try again later.");
+            })
 
-}, []);
+    }, []);
 
-    return(
-    <div>
-        <h1>Classes</h1>
-        {error && <p style={{ color: "red" }}>{error}</p>}
-        {data.map((item)=>{
-            <li key={item._id}>{item.name}</li>
+    return (
+        <div>
+            <h1>Classes</h1>
+            {error && <p style={{ color: "red" }}>{error}</p>}
+            {data.map((item) => {
+                return <li key={item._id}>{item.name}</li>;
+            })}
 
-        })}
 
-    </div>
+        </div>
 
 
     )
 }
+
 export default HomePage
