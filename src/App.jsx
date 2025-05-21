62f7f2a0274aa7cb89a61c296ba0c33c22fb7869
José Baldino <josemanuelbaldino@gmail.com>
2025-05-21 10:17:44 +0100

    create auth context

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import { useState } from 'react'
 import './App.css'
 import axios from "axios";
-import {Routes, Route} from "react-router-dom"
+import { Routes, Route } from "react-router-dom"
+
+import Navbar from './components/Navbar';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-   
-     
+      <Navbar />
+      <h1>hello</h1>
     </>
   )
 }
