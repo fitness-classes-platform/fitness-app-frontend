3264710819701880a7344b561380ec5cea809bf1
Mariaesteves <mariaferraesteves@hotmail.com>
2025-05-27 10:03:58 +0100

    change css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,12 @@
-
-function Footer(){
-
-    return
-    <>
-    </>
-}
-
-export default Footer
+function Footer() {
+    return (
+      <section className="footer">
+        <div className="footer-links">
+          <a href="https://github.com/orgs/fitness-classes-platform/repositories" target="_blank" rel="noopener noreferrer" className="github-link">GitHub Repo</a>
+        </div>
+      </section>
+  
+    );
+  }
+  
+  export default Footer;
